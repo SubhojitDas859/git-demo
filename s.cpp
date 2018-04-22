3fd91dec47a80da6d58d621bf77f50b97c7d398a
subhojit <sdas78171@gmail.com>
2018-04-22 20:02:14 +0530

    initial commmit

---
+++ s.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h> 
+#include<conio.h> 
+class base 
+{ 
+int val1,val2; 
+public: 
+void get() 
+{ 
+cout<<"Enter Two Values:"; 
+cin>>val1>>val2; 
+} 
+friend float mean(base ob); 
+}; 
+float mean(base ob) 
+{ 
+return float(ob.val1+ob.val2)/2; 
+} 
+int main() 
+{ 
+base obj; 
+obj.get(); 
+cout<<"\n Mean value is:"<<mean(obj); 
+getch(); 
+return 0; 
+}
